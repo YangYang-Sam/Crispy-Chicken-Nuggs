927d2ad2542015d9d04a1d922ad09fd2ba631b2f
ShilongAn <46959079+ShilongAn@users.noreply.github.com>
2019-02-05 00:31:13 +0000

    Haven't finished
    The Auto Door haven't achieced.

---
+++ Assets/Scripts/Tigger.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    private Door m_Door;
+    private bool enterCollide = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Door = GameObject.Find("Doorshaft").GetComponent<Door>();
+    }
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("enter");
+        enterCollide = true;
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        Debug.Log("exit");
+        enterCollide = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (enterCollide)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (m_Door.GetIsOpen())
+                {
+                    m_Door.CloseDoorMethod();
+
+                }
+                else
+                {
+                    m_Door.OpenDoorMethod();
+                }
+            }
+        }
+
+    }
+}
