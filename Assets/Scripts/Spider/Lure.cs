c42129987eb316a6bb0906f853840e1e4e9435d8
Corneilus <44402292+Cyoung117@users.noreply.github.com>
2019-02-10 19:14:34 +0000

    Updated spider
    Added final spide rmodel with relevant scripts

---
+++ Assets/Scripts/Spider/Lure.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lure : MonoBehaviour {
+
+    public Patrol patrol;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        patrol.MoveToPosition(collision.contacts[0].point);
+
+
+    }
+
+
+	
+}
--- Assets/Scripts/Spider/Patrol.cs
+++ Assets/Scripts/Spider/Patrol.cs
@@ -6,31 +6,35 @@ public class Patrol : MonoBehaviour
 {
     //define the public classes that will be used 
     public float speed;
+    public float speedMultiplier;
     public float waitTime;
     public float startWaitTime;
     public Transform[] moveSpots;
     public Transform player;
     private int randomSpot;
     public bool isPatrolling;
+    public bool isMovingToPosition;
     public bool isFollowing;
     private Animator animator;
+    [SerializeField]
+    private GameObject lure;
 
-    // On start select a random spot to move to
+    // On start make sure the animator idle animation begins then aqquire random spot to move to but don't move until given command
     void Start()
     {
         animator = GetComponent<Animator>();
         waitTime = startWaitTime;
         randomSpot = Random.Range(0, moveSpots.Length);
     }
-    
-    // Change position and move towards new spot on update
+
+    // On update use both bools to define whether to follow the player or follow the patrol points
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             isFollowing = true;
             isPatrolling = false;
-           
+
         }
 
         if (isFollowing == true)
@@ -47,17 +51,25 @@ public class Patrol : MonoBehaviour
         if (isPatrolling == true)
         {
             EnemyPatrol();
+
         }
+
+       
+
+        if (isMovingToPosition == true)
+        {
+            MoveToPosition(lure.gameObject.transform.position);
+        }
+    
+
 
         animator.SetBool("isPatrolling", isPatrolling);
         animator.SetBool("isFollowing", isFollowing);
+        animator.SetBool("isFollowing", isMovingToPosition);
 
 
-        {
-            
-        }
     }
-
+    // On enemy patrol, move to random spot  in relation to the determined speed, also rotate spider to look at predetermined move spot
     void EnemyPatrol()
     {
         transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
@@ -78,12 +90,31 @@ public class Patrol : MonoBehaviour
 
         }
     }
-
+    // On follow player move toward player position and also look at player when doing so
     void FollowPlayer()
     {
-        transform.position = Vector3.MoveTowards(transform.position, player.position , speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, player.position , speed * speedMultiplier * Time.deltaTime);
         transform.LookAt(player);
     }
-        
-    
+
+    public void MoveToPosition(Vector3 target)
+    {
+        Debug.Log("Moving to position");
+        isPatrolling = false;
+        isFollowing = false;
+        isMovingToPosition = true;
+
+        if (isMovingToPosition == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * speedMultiplier * Time.deltaTime);
+            transform.LookAt(target);
+        }
+
+        if (Vector3.Distance(transform.position, target) < 0.2f)
+        {
+            isMovingToPosition = false;
+        }
+
+    }
+ 
 }
