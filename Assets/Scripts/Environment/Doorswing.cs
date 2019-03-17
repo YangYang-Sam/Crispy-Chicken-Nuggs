d60b046d5c934246b70c7796aa43b079b0734152
Yang <1046276672@qq.com>
2019-03-17 23:00:16 +0000

    Scripit light and door

---
+++ Assets/Scripts/Environment/Doorswing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doorswing : MonoBehaviour
+{
+    public float angle=20f;
+    public float swingfreq = 10f;
+    public Transform pivot;
+
+    private float a;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        a = angle-Mathf.PingPong(Time.time*swingfreq, angle);
+        pivot.localRotation = Quaternion.Euler(0, a, 0);
+    }
+}
---
+++ Assets/Scripts/Environment/OpenDoor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    [SerializeField] Transform doorPivot;
+    [SerializeField] float openAngle;
+    [SerializeField] float openTime;
+
+    public bool doorOpened;
+    public Doorswing doorswing;
+    public Lightflickering lt;
+    bool opening;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !opening)
+        {
+            doorswing.enabled = false;
+            lt.enabled = false;
+            Controldoor();
+        }
+    }
+
+    public void Controldoor()
+    {
+        doorOpened = !doorOpened;
+        StartCoroutine(OpenOrClose(doorOpened));
+    }
+
+
+    IEnumerator OpenOrClose(bool open)
+    {
+        var time = Time.time;
+        var timePassed = 0f;
+
+        opening = true;
+
+        while (timePassed < openTime)
+        {
+            timePassed = Time.time - time;
+            var angle = 0f;
+
+            if (open)
+            {
+                angle = Mathf.Lerp(0, openAngle, timePassed / openTime);
+            }
+            else
+            {
+                angle = Mathf.Lerp(openAngle, 0, timePassed / openTime);
+            }
+
+            doorPivot.localRotation = Quaternion.Euler(0, angle, 0);
+            yield return null;
+        }
+
+        opening = false;
+    }
+}
