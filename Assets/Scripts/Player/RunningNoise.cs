3eeaa016624a959760c0967d4475adfc12b3710e
Yang <1046276672@qq.com>
2019-03-25 01:31:34 +0000

    Big Update for spider AI

---
+++ Assets/Scripts/Player/RunningNoise.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningNoise : MonoBehaviour
+{
+    private Stamina s;
+    private ChasingLure runningsound;
+    public ChasingTarget target;
+
+    void Start()
+    {
+        s=GetComponent<Stamina>();
+        runningsound = FindObjectOfType<ChasingLure>();
+    }
+
+    void Update()
+    {
+        if (s.isRunning)
+        {
+            target.position = transform.position;
+            runningsound.UpdateTarget(target);
+        }
+    }
+}
