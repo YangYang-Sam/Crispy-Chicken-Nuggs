1b95568501b1be3e7b33ad5422d2b5c8a7fbeae2
EliotHynd <44072377+EliotHynd@users.noreply.github.com>
2019-04-14 15:25:00 +0100

    ImportingAssetsEtc

---
+++ Assets/EliotStuff/PusleWithSound.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PusleWithSound : MonoBehaviour
+{
+    public SonarFxDescriptor sonarcontrol;
+    public float sonarrate = 2f;
+    private float nextsonar;
+
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+
+
+
+        if (Time.time > nextsonar)
+        {
+            nextsonar = Time.time + sonarrate;
+            sonarcontrol.origin = transform.position;
+            SonarFx.Instance.StartSonar(sonarcontrol);
+
+
+        }
+
+
+        
+
+
+
+
+
+    }
+
+
+}
