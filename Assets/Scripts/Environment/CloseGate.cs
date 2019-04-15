b043a36bb34cad163985b577b4490ec8f822473c
Yang <1046276672@qq.com>
2019-04-15 16:51:52 +0100

    update Gate close

---
+++ Assets/Scripts/Environment/CloseGate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseGate : MonoBehaviour
+{
+    public Transform pivot;
+    public float closetime;
+    public float closeangle;
+    public float oldangle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Close());
+            Destroy(gameObject,closetime);
+        }
+    }
+
+    IEnumerator Close()
+    {
+        var time = Time.time;
+        var timePassed = 0f;
+
+
+        while (timePassed < closetime)
+        {
+            timePassed = Time.time - time;
+            var angle = 0f;
+
+
+            angle = Mathf.Lerp(oldangle, oldangle+closeangle, timePassed / closetime);
+
+            pivot.localRotation= Quaternion.Euler(0, angle, 0);
+            yield return null;
+        }
+    }
+}
