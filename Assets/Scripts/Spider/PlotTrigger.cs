59d20ceb19e4480545a1cc857b0cbbd99b6855f0
Corry Young <corry17young@gmail.com>
2019-04-16 13:34:06 +0100

    Added audio to animation scene

--- Assets/Scripts/Spider/PlotTrigger.cs
+++ Assets/Scripts/Spider/PlotTrigger.cs
@@ -7,11 +7,13 @@ public class PlotTrigger : MonoBehaviour
     public GameObject actor;
     public GameObject light;
     public GameObject anim;
+    public AudioSource windowBreak;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            windowBreak.Play();
             anim.SetActive (true);
             light.SetActive(true);
             Destroy(actor, 12f);
