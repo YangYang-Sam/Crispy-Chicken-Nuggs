20607afe61207c5ddcbc80e810e125028ce2f87f
Yang <1046276672@qq.com>
2019-04-18 17:10:12 +0100

    fixed bug of monitor

--- Assets/Scripts/Environment/Monitor.cs
+++ Assets/Scripts/Environment/Monitor.cs
@@ -43,7 +43,7 @@ public class Monitor : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")&&playplot==false)
         {
             heartStop.Stop();
             heartLoop.Play();
