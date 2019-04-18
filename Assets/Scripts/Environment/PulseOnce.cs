3a8c41410436886e8e94afebc13a8a2354df75d3
Yang <1046276672@qq.com>
2019-04-18 22:18:04 +0100

    Delete something useless, change the wave of the rock

---
+++ Assets/Scripts/Environment/PulseOnce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseOnce : MonoBehaviour
+{
+    public SonarFxDescriptor sonarcontrol;
+
+    void Start()
+    {
+        sonarcontrol.origin = transform.position;
+        SonarFx.Instance.StartSonar(sonarcontrol);
+    }
+
+    void Update()
+    {
+        
+    }
+}
